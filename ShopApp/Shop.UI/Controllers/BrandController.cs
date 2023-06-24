b92c3afc24df74b8113216fdf78535ef0492d7a9
Elchin Aghayev <Elchin@code.local>
2023-06-24 19:15:44 +0400

    project added

---
+++ ShopApp/Shop.UI/Controllers/BrandController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Shop.UI.Models;
+
+namespace Shop.UI.Controllers
+{
+    public class BrandController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                using(var response = await client.GetAsync("https://localhost:7171/api/Brands/all"))
+                {
+                    if(response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var data = JsonConvert.DeserializeObject<List<BrandGetAllİtemResponse>>(content);
+
+                        return View(data);
+                    }
+                }
+            }
+            return View("error");
+        }
+    }
+}
---
+++ ShopApp/Shop.UI/Models/BrandGetAllResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Shop.UI.Models
+{
+    public class BrandGetAllİtemResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
